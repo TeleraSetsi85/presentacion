e4ef1c0f95982759348242d9603bf1cdc43039a0
Diego Alfonso <darellano1@ucol.mx>
2025-01-13 21:16:42 -0600

    Bases para iniciar el proyecto

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -1,10 +1,15 @@
+import Header from './components/Header';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 function App (){
   return(
     <>
-    
+      <Header />
+      <NavBar />
+      <Footer />
     </>
-  )
+  );
 }
 
 export default App;
