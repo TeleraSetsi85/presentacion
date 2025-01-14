bef7ad5501135c627ecbef63fcf6956e3b5a2e87
Diego Alfonso <darellano1@ucol.mx>
2025-01-13 21:06:16 -0600

    Barrida de archivos inecesarios

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -1,18 +1,10 @@
-import './App.css'
-import Header from './components/Header';
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
 
-function App() {
-  
-
-  return (
+function App (){
+  return(
     <>
-      <Header />
-      <NavBar />
-      <Footer />
+    
     </>
   )
 }
 
-export default App
+export default App;
