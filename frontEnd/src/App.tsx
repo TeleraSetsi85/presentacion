850f1cb0f858e8b0148b4cac260440bbed9d2d1b
Diego Alfonso <darellano1@ucol.mx>
2025-01-13 20:21:34 -0600

    s

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css'
+import Header from './components/Header';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 function App() {
   
 
   return (
     <>
-      
+      <Header />
+      <NavBar />
+      <Footer />
     </>
   )
 }
